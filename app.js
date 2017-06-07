6f526ddd8eb8152bead934c2e57ea7522b5d93aa
JJ <jj.spetseris@yahoo.com>
2017-06-07 11:16:56 -0500

    step 0-5 complete

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
 // restaurant reviewer with express
 
+// imports
 var express = require('express');
 var bp = require('body-parser');
 var promise = require('bluebird');
+var pgp = require('pg-promise')({
+  promiseLib: promise
+});
 
+
+/************
+*   SETUP
+*************/
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+var db = pgp({database: 'restaurant'});
+
+app.set('view engine', 'hbs');
+
+app.use(bp.urlencoded({ extended: false }));
+app.use('/static/', express.static('public'));
+
+
+/************
+*   ROUTES
+*************/
+// homepage route
+app.get('/', function(req, resp) {
+  resp.render('homepage.hbs', {title: 'homepage'})
+})
+
+// search route (displays after user does a search)
+app.get('/search', function(req, resp, next) {
+  let search = req.query.search
+  let query = "SELECT * FROM restaurant WHERE restaurant.name ILIKE '%$1#%'";
+  db.any(query, search)
+    .then(function(results) {
+      resp.render('results.hbs', {
+        title: 'Search Results',
+        results: results
+      });
+    })
+    .catch(next);
+});
+
+// restaurant page route
+app.get('/restaurant/:id', function(req, resp, next) {
+  let id = req.params.id;
+  let query = "SELECT * FROM restaurant WHERE restaurant.id=$1";
+  db.one(query, id)
+    .then(function(results) {
+      resp.render('restaurant.hbs', {
+        title: results.name,
+        results: results,
+        address: results.address || 'none'
+      });
+    })
+    .catch(next);
+});
 
 // server listen
 app.listen(8000, function() {
